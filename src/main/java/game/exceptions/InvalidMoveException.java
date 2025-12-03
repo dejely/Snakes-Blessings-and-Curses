4db7be98202f2264f72ae3a79bc03ff38b04589a
Ralfreeee <ralphryanescabarte@gmail.com>
2025-12-03 21:42:20 +0800

    Exceptions Code
    Began adding exception-related code.

--- src/main/java/game/exceptions/InvalidMoveException.java
+++ src/main/java/game/exceptions/InvalidMoveException.java
@@ -1,9 +1,20 @@
 package game.exceptions;
 
+/**
+ * Thrown when a player's attempted move violates
+ * the game rules or is not permitted at the moment.
+ */
+
 public class InvalidMoveException {
 
 	public InvalidMoveException() {
-		// TODO Auto-generated constructor stub
+		public InvalidMoveException() {
+			super("Invalid move attempted.");
+		}
+
+		public InvalidMoveException(String message) {
+			super(message);
+		}
 	}
 
 }
