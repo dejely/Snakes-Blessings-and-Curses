687d370f16ea69f42ac44a41d7499f5036dbdabf
dejely <captain.jelly@yahoo.com>
2025-12-03 18:57:04 +0800

    Test for Main.java
    Configured main java for default GUI, migrate to UI dir

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,21 +1,42 @@
 package game;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel; 
 
 /**
  * This would be our Caller for
  * All the Subsystems we made per packages(file sub directories)
- * As mentioned in Design Patterns Facade
+ * As mentioned in Design Patterns: Facade
  */
 
 public class Main { 
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame window = new JFrame("Snake-Blessings and Curses");
+		JPanel panel = new JPanel(new BorderLayout());
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(400, 400);
+		window.setResizable(true);
+		
+        JLabel greeting = new JLabel("Hello, user!", JLabel.CENTER);
+        JLabel message = new JLabel("Welcome to Snake-Blessings and Curses", JLabel.CENTER);
+        
+        panel.add(greeting, BorderLayout.NORTH);
+        panel.add(message, BorderLayout.CENTER);
+		
+		window.add(panel);
+		window.pack();
+		window.setVisible(true);
+		
+		
 	}
+	
+	
 
 }
